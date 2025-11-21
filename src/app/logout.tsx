64668fe33ef19c69282fa10675422d9ee056c525
himanshuhe981 <techstryker1@gmail.com>
2025-11-22 01:02:14 +0530

    authentication secured with trpc 04

---
+++ src/app/logout.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { authClient } from "@/lib/auth-client"
+import { useRouter } from "next/navigation";
+
+export const  LogoutButton = () =>{
+
+    const router = useRouter();
+
+    return (<div>
+        <Button onClick={() => authClient.signOut({
+            fetchOptions: {
+                onSuccess: () => {
+                    router.push("/login");
+                }
+            }
+        })}>
+        Logout
+    </Button>
+    </div>);
+};
+
+export default LogoutButton;
