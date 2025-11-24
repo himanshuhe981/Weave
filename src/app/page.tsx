1996deb8de353baa726c637d444ae5416a185c6d
himanshuhe981 <techstryker1@gmail.com>
2025-11-24 12:39:57 +0530

    07: ai providers ..added fro gemini anthropic and open ai only gemini working due to api key isssue ..ensure you remove teh non woking ones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,16 @@ const Page = () => {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
   const {data} = useQuery(trpc.getWorkflows.queryOptions());
-  const create = useMutation(trpc.createWorkflow.mutationOptions({
+
+  const testAi = useMutation(trpc.testAi.mutationOptions({
     onSuccess: () => {
       toast.success("Job queued")
+    }
+  }));
+
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Ai Job queued")
     }
   }));
 
@@ -26,6 +33,9 @@ const Page = () => {
     <div>
       {JSON.stringify(data,null,2)}
     </div>
+    <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+      Test Ai
+    </Button>
     <Button disabled={create.isPending} onClick={() => create.mutate()} >
       Create workflow
     </Button>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,18 @@
 import prisma from '@/lib/db';
-import { createTRPCRouter, protectedProcedure } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import { inngest } from '@/inngest/client';
 import { email } from 'zod';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
 
 
 export const appRouter = createTRPCRouter({
+  testAi: baseProcedure.mutation(async () => {
+    await inngest.send({
+    name: "execute/ai",
+  });
+  return {success: true, message: "Job queued"}
+}),
   getWorkflows: protectedProcedure.query(({ctx}) => {
       return prisma.workflow.findMany();
     }),
