b9a792cf224039628c83e3c03356eec3c0a03d6d
himanshuhe981 <techstryker1@gmail.com>
2026-01-30 19:46:49 +0530

    19: node variables (fixed the overwriting of next http node overwritten by prevous node data)

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -28,6 +28,13 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
+
+    variableName: z
+        .string()
+        .min(1, {message: "Variable name is required"})
+        .regex(/^[A-Za-z_$][A-Za-z0-9_$]*$/, {
+            message: "Varible name must start with a letter or underscore and contain only letters, numbers, and underscores",
+        }),  
     endpoint: z.url({ message: "Please enter a valid URL"}),
     method: z.enum(["GET","POST","PUT","PATCH","DELETE"]),
     body: z
@@ -55,6 +62,7 @@ export const HttpRequestDialog = ({
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
+            variableName: defaultValues.variableName || "",
             endpoint: defaultValues.endpoint || "",
             method: defaultValues.method || "GET",
             body: defaultValues.body || "",
@@ -66,6 +74,7 @@ export const HttpRequestDialog = ({
     useEffect(() => {
         if(open){
             form.reset({
+                variableName: defaultValues.variableName || "",
                  endpoint: defaultValues.endpoint || "",
                  method: defaultValues.method || "GET",
                  body: defaultValues.body || "",
@@ -73,6 +82,7 @@ export const HttpRequestDialog = ({
         }
     },[open, defaultValues,form]);
 
+    const watchVariableName = form.watch("variableName") || "myApiCall";
     const watchMethod = form.watch("method");
     const showBodyField = ["POST","PUT","PATCH"].includes
     (watchMethod);
@@ -93,6 +103,28 @@ export const HttpRequestDialog = ({
                     <form onSubmit={form.handleSubmit(handleSubmit)}
                     className="space-y-8 mt-4"
                     >
+                        <FormField
+                            control={form.control}
+                            name="variableName"
+                            render = {({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Variable Name</FormLabel>
+                                    <FormControl>
+                                       <Input
+                                        placeholder="myApiCall"
+                                        {...field}
+                                    />
+                                    </FormControl>
+                                   
+                                   <FormDescription>
+                                        Use this name to reference the result in other nodes:{" "}
+                                        {`{{${watchVariableName}.httpResponse.data}}`}
+                                    </FormDescription>
+                                   <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+
                         <FormField
                             control={form.control}
                             name="method"
