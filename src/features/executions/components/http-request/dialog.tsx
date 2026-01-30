37119c28109f56c2776a1007bbe0b97069aaa48b
himanshuhe981 <techstryker1@gmail.com>
2026-01-30 06:34:42 +0530

    18: node executions (mainly http request node)

--- src/features/executions/components/http-request/dialog.tsx
+++ src/features/executions/components/http-request/dialog.tsx
@@ -36,32 +36,29 @@ const formSchema = z.object({
         // .refine() TODO
 });
 
-export type FormType = z.infer<typeof formSchema>;
+export type HttpRequestFormValues = z.infer<typeof formSchema>;
 
 interface Props {
     open: boolean;
     onOpenChange: (open: boolean) => void;
     onSubmit: (values: z.infer<typeof formSchema>) => void;
-    defaultEndpoint?: string;
-    defaultMethod?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-    defaultBody?: string;
+    defaultValues?: Partial<HttpRequestFormValues>;
 };
 
 export const HttpRequestDialog = ({
     open,
     onOpenChange,
     onSubmit,
-    defaultEndpoint = "",
-    defaultMethod = "GET",
-    defaultBody = "",
+    defaultValues = {},
+
 } : Props ) => {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            endpoint: defaultEndpoint,
-            method: defaultMethod,
-            body: defaultBody,
-        }
+            endpoint: defaultValues.endpoint || "",
+            method: defaultValues.method || "GET",
+            body: defaultValues.body || "",
+        },
     });
 
     // Reset form values when dialog opens with new defaults
@@ -69,12 +66,12 @@ export const HttpRequestDialog = ({
     useEffect(() => {
         if(open){
             form.reset({
-                endpoint: defaultEndpoint,
-                method: defaultMethod,
-                body: defaultBody,
+                 endpoint: defaultValues.endpoint || "",
+                 method: defaultValues.method || "GET",
+                 body: defaultValues.body || "",
             });
         }
-    },[open, defaultEndpoint,defaultMethod,defaultBody,form]);
+    },[open, defaultValues,form]);
 
     const watchMethod = form.watch("method");
     const showBodyField = ["POST","PUT","PATCH"].includes
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,68 +1,50 @@
-import prisma from "@/lib/db";
+import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { generateText } from "ai";
-import {createOpenAI} from "@ai-sdk/openai"
-import {createAnthropic} from "@ai-sdk/anthropic"
+import prisma from "@/lib/db";
+import { topologicalSort } from "./utils";
+import { NodeType } from "@/generated/prisma/enums";
+import { getExecutor } from "@/features/executions/lib/executor-registry";
 
-const google = createGoogleGenerativeAI();
-const openai = createOpenAI();
-const anthropic = createAnthropic();
 
-export const execute  = inngest.createFunction(
-  { id: "execute-ai" },
-  { event: "execute/ai" },
+export const executeWorkflow  = inngest.createFunction(
+  { id: "execute-workflow" },
+  { event: "workflows/execute.workflow" },
   async ({ event, step }) => {
-    await step.sleep("pretend","5s");
+    const workflowId = event.data.workflowId;
+ 
+    if(!workflowId) {
+      throw new NonRetriableError("Workflow ID is  missing");
+    }
 
-    const {steps: geminiSteps}= await step.ai.wrap(
-    "gemini-generate-text",
-    generateText,
-    {
-      model:google("gemini-2.5-flash"),
-      system:"You are a helpful assistant.",
-      prompt: "what is 2 +2 ?",
-      experimental_telemetry: {
-      isEnabled: true,
-      recordInputs: true,
-      recordOutputs: true,
-  },
+    const sortedNodes = await step.run("prepare-workflow", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: { id: workflowId },
+        include: {
+          nodes: true,
+          connections: true,
+        }
+      });
+      return topologicalSort(workflow.nodes, workflow.connections);
+    });
+
+    // Initialize context with any initial data from the trigger
+    let context = event.data.initialData || {};
+
+    //execute each node 
+    for (const node of sortedNodes){
+      const executor = getExecutor(node.type as NodeType);
+      context = await executor({
+        data: node.data as Record<string, unknown>,
+        nodeId: node.id,
+        context,
+        step,
+      })
     }
-  );
-    const {steps: openaiSteps}= await step.ai.wrap(
-    "openai-generate-text",
-    generateText,
-    {
-      model:openai("gpt-4"),
-      system:"You are a helpful assistant.",
-      prompt: "what is 2 +2 ?"
-      ,
-      experimental_telemetry: {
-      isEnabled: true,
-      recordInputs: true,
-      recordOutputs: true,
-  },
-    }
-  );
-    const {steps: anthropicSteps}= await step.ai.wrap(
-    "anthropic-generate-text",
-    generateText,
-    {
-      model:anthropic("claude-3-5-haiku-latest"),
-      system:"You are a helpful assistant.",
-      prompt: "what is 2 +2 ?"
-      ,
-      experimental_telemetry: {
-      isEnabled: true,
-      recordInputs: true,
-      recordOutputs: true,
-  },
-    }
-  );
-  return {
-    geminiSteps,
-    openaiSteps,
-    anthropicSteps,
-  };   
+
+    return { 
+        workflowId,
+        result: context,
+     };
+    
   },
 );
