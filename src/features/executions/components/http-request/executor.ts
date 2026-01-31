68457e69f79573bd2c2370f514e02cc6fb2288f7
himanshuhe981 <techstryker1@gmail.com>
2026-01-31 18:02:21 +0530

    20: Node templating for http request node fixed data poulation among nodes taht occur in serial one after other

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -1,12 +1,19 @@
 import type { NodeExecutor } from "@/features/executions/types" 
 import { NonRetriableError } from "inngest";
 import ky, {type Options as KyOptions } from "ky";
+import Handlebars from "handlebars";
+ 
+Handlebars.registerHelper("json", (context) => {
+    const jsonString = JSON.stringify(context,null,2);
+    const safeString = new Handlebars.SafeString(jsonString);
 
+    return safeString;
+});
 
 type HttpRequestData = {
-    variableName?: string,
-    endpoint?: string;
-    method?: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+    variableName: string,
+    endpoint: string;
+    method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
     body?: string;
 };
 
@@ -28,16 +35,22 @@ async ({
         // TODO: Publish "error" state for http request 
         throw new NonRetriableError("Variable name not configured");
     }
+    if (!data.method) {
+        // TODO: Publish "error" state for http request 
+        throw new NonRetriableError("Method not configured");
+    }
 
     // const result = await step.run("http-request", async () => context);
 
     const result = await step.run("http-request", async () => {
-        const endpoint = data.endpoint!;
-        const method = data.method || "GET";
+        const endpoint = Handlebars.compile(data.endpoint)(context);
+        const method = data.method;
         const options: KyOptions = { method };
 
         if (["POST","PUT","PATCH"].includes(method)) {
-            options.body = data.body;
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved);
+            options.body = resolved;
             options.headers = {
                 "Content-Type": "application/json",
             };
@@ -57,18 +70,12 @@ async ({
             }
         }
 
-        if (data.variableName) {
             return {
             ...context,
             [data.variableName]: responsePayload,
             }
-        }
 
-        //Fallback to direct httpResponse for  backward compatibilty
-        return {
-            ...context,
-            ...responsePayload
-        }
+       
 
     });
 
