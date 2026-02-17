342b8c8db50935da6c96ee22f2cd16c75b127a33
himanshuhe981 <techstryker1@gmail.com>
2026-02-18 01:18:45 +0530

    29: github google auth

--- src/features/auth/componenets/register-form.tsx
+++ src/features/auth/componenets/register-form.tsx
@@ -75,6 +75,32 @@ export function RegisterForm() {
     )
 };
 
+const signInGithub = async () => {
+        await authClient.signIn.social({
+            provider: "github",
+        },{
+            onSuccess:() => {
+                router.push("/")                
+            },
+            onError: () => {
+                toast.error("Something went wrong");
+            },
+        });
+    };
+
+    const signInGoogle = async () => {
+        await authClient.signIn.social({
+            provider: "google",
+        },{
+            onSuccess:() => {
+                router.push("/")                
+            },
+            onError: () => {
+                toast.error("Something went wrong");
+            },
+        });
+    };
+
     const isPending = form.formState.isSubmitting;
 
     return (
@@ -94,6 +120,7 @@ export function RegisterForm() {
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
                                     <Button
+                                    onClick={signInGithub}
                                     variant={'outline'}
                                     className="w-full"
                                     type="button"
@@ -103,6 +130,7 @@ export function RegisterForm() {
                                         Continue with Github
                                     </Button>
                                     <Button
+                                    onClick={signInGoogle}
                                     variant={'outline'}
                                     className="w-full"
                                     type="button"
